c4bfa46f2a6fe98ffbe7dae4538d4518fbe2abc8
Tas de Foin <douzmaur@gmail.com>
2021-11-09 20:11:12 +0100

    Salut je suis un panneau

--- hamstergram_api.py
+++ hamstergram_api.py
@@ -1,17 +1,6 @@
 """
-Salut je suis un panneau
+API de messagerie pour Hamstramgram.
+
+GitHub : https://github.com/Tadf0in/Hamstergram
 """
 
-
-
-def add_friend(friend_name):
-    """ Fonction pour ajouter un utilisateur en ami.
-
-    In : friend_name (str) : correspond au nom d'utilisateur d'un utilisateur enregistré
-    Out :
-    Retourne -1 si les conditions ne sont pas respectées
-    """
-    if type(friend_name) != str():
-        return -1
-
-print(add_friend(12))
