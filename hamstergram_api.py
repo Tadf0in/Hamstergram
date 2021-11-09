427019dbf3d89a5aa036eda9da0b293868962e38
Tas de Foin <douzmaur@gmail.com>
2021-11-09 20:16:03 +0100

    def créer connexion

--- hamstergram_api.py
+++ hamstergram_api.py
@@ -4,5 +4,23 @@ API de messagerie pour Hamstramgram.
 GitHub : https://github.com/Tadf0in/Hamstergram
 """
 
+def _creer_connexion(db_file):
+    """ Crée une connexion à la base de données SQLite spécifiée par db_file.
+        Le fichier est créé s'il n'existe pas.
+
+    In : db_file (str) : Chemin vers un fichier .db
+    Out : objet connexion ou None
+    """
+    try:
+        conn = sqlite3.connect(db_file)
+        conn.execute("PRAGMA foreign_keys = 1")
+        return conn
+    except sqlite3.Error as e:
+        print(e)
+
+    return None
+
+
+
 if __name__ == '__main__':
     assert 1 == 1
