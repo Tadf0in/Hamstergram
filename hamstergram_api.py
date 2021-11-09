70bd0d9996f23bc04dead6c4e10d984ff7abc04b
Tas de Foin <douzmaur@gmail.com>
2021-11-09 20:10:14 +0100

    Création fonction

--- hamstergram_api.py
+++ hamstergram_api.py
@@ -1,1 +1,16 @@
+"""
+API de messagerie pour Hamstramgram.
 
+GitHub : https://github.com/Tadf0in/Hamstergram
+"""
+
+
+def add_friend(friend_name):
+    """ Fonction pour ajouter un utilisateur en ami.
+
+    In : friend_name (str) : correspond au nom d'utilisateur d'un utilisateur enregistré
+    Out :
+    Retourne -1 si les conditions ne sont pas respectées
+    """
+    if type(friend_name) != str():
+        return -1
